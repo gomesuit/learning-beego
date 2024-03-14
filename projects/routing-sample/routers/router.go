831d848201bd642b1661d50d32a9e5ad51524038
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-03-14 19:05:13 +0900

    save

--- projects/routing-sample/routers/router.go
+++ projects/routing-sample/routers/router.go
@@ -1,10 +1,11 @@
 package routers
 
 import (
-	"routing-sample/controllers"
 	"github.com/astaxie/beego"
+
+	"routing-sample/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
